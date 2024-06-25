f26d3a9a10f21261dee9f5dbd1172f0c203fa0a8
Luke <neumalu1@fel.cvut.cz>
2024-06-25 09:02:23 +0200

    EfficientViT L1

---
+++ rci_scripts/efficientvit-l1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=efficientvit-orig
+#SBATCH --time=72:00:00
+#SBATCH --partition=amdgpulong
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:4
+#SBATCH --mem=128gb
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=14
+#SBATCH --output=/home/neumalu1/data/efficientvit/efficientvit_%j.out
+# -------------------------------
+
+ml fosscuda/2020b
+ml Anaconda3
+
+source activate efficientvit
+export OMP_NUM_THREADS=14
+torchrun --nnodes 1 --nproc_per_node=4 train_cls_model.py configs/cls/imagenet/l1.yaml --amp bf16 --path .exp/cls/imagenet/l1_r224_trainingcode/
+
+
+
