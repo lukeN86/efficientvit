97f38eef7fcca1164e1ffe507851ade908a2d221
Luke <neumalu1@fel.cvut.cz>
2024-07-26 10:16:09 +0200

    EfficientViT L3

--- rci_scripts/efficientvit-l1-b512.sh
+++ rci_scripts/efficientvit-l1-b512.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=efficientvit
 #SBATCH --time=72:00:00
-#SBATCH --partition=amdgpuelong
+#SBATCH --partition=amdgpulong
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:4
 #SBATCH --mem=128gb
